23ab79fab90da75dcee3a3aa1b3803501217a0e7
Daniel Kowalski <77574206+k0wal45@users.noreply.github.com>
2023-07-24 11:59:21 +0200

    Update PromoInfo.jsx

--- src/components/PromoInfo.jsx
+++ src/components/PromoInfo.jsx
@@ -1,9 +1,22 @@
 import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faX } from "@fortawesome/free-solid-svg-icons"
+import { useState } from "react"
 
 const PromoInfo = () => {
+
+  const [visibility, setVisibility] = useState('block')
+
+  const handleClick = (e) => {
+    setVisibility('hidden')
+  }
+
   return (
-    <div className="flex justify-center items-center  bg-warning w-full z-50">
+    <div className={`flex justify-between items-center  bg-warning w-full h-min z-50 px-4 text-white ${visibility}`}>
+      <div></div>
       <p>Do końca sierpnia obowiązuje <strong>PROMOCJA NA WSZYSTKO 25%</strong>, <Link to='/kontakt' className="underline hover:scale-110">skontaktuj się z nami już teraz</Link></p>
+      <FontAwesomeIcon icon={faX}  onClick={handleClick}/>
+
     </div>
   )
 }
